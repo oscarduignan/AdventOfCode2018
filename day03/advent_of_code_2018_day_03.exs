ec8d7e7353137d99b46367c60f93a77c319939d9
Oscar Duignan <oscarduignan@gmail.com>
2018-12-04 01:58:37 +0000

    refactor(day03): for readability
    Runs a bit slower overall but part2 is much faster to run and it's much easier to read

--- day03/advent_of_code_2018_day_03.exs
+++ day03/advent_of_code_2018_day_03.exs
@@ -1,32 +1,4 @@
 defmodule Day3 do
-  def reject_intersecting(rectangles, area) do
-    rectangles
-    |> Enum.filter(fn %{area: rectangle_area} ->
-      MapSet.disjoint?(area, rectangle_area)
-    end)
-  end
-
-  def find_intersecting_area([]), do: MapSet.new()
-
-  def find_intersecting_area([current_rectangle | other_rectangles]) do
-    current_rectangle
-    |> find_intersecting_area(other_rectangles)
-    |> MapSet.union(find_intersecting_area(other_rectangles))
-  end
-
-  def find_intersecting_area(current_rectangle, other_rectangles) do
-    other_rectangles
-    |> Enum.reduce(MapSet.new(), fn other_rectangle, intersecting_area ->
-      MapSet.union(
-        intersecting_area,
-        MapSet.intersection(
-          current_rectangle.area,
-          other_rectangle.area
-        )
-      )
-    end)
-  end
-
   def parse_input(input) do
     input
     |> String.split("\n", trim: true)
@@ -37,30 +9,42 @@ defmodule Day3 do
     line
     |> String.split([" ", ",", "x", ":", "@", "#"], trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> into_map
-    |> put_area
+    |> line_to_tuple
   end
 
-  defp into_map([id, left, top, width, height]) do
-    %{
-      id: id,
-      left: left,
-      top: top,
-      width: width,
-      height: height
-    }
+  defp line_to_tuple([id, left, top, width, height]) do
+    {id,
+     Enum.reduce(Range.new(left + 1, left + width), MapSet.new(), fn x, area ->
+       Enum.reduce(Range.new(top + 1, top + height), area, fn y, area ->
+         MapSet.put(area, {x, y})
+       end)
+     end)}
   end
 
-  defp put_area(rectangle) do
-    rectangle
-    |> Map.put(:area, area(rectangle))
+  def find_intersecting_area([current_rectangle | other_rectangles]) do
+    current_rectangle
+    |> find_intersecting_area(other_rectangles)
+    |> MapSet.union(find_intersecting_area(other_rectangles))
   end
 
-  defp area(%{top: top, left: left, width: width, height: height}) do
-    Range.new(left + 1, left + width)
-    |> Enum.reduce(MapSet.new(), fn x, acc ->
-      Range.new(top + 1, top + height)
-      |> Enum.reduce(acc, fn y, acc -> acc |> MapSet.put({x, y}) end)
+  def find_intersecting_area([]) do
+    MapSet.new()
+  end
+
+  def find_intersecting_area({_, outer_area}, rectangles) do
+    Enum.reduce(rectangles, MapSet.new(), fn {_, inner_area}, intersecting_area ->
+      MapSet.union(
+        intersecting_area,
+        MapSet.intersection(outer_area, inner_area)
+      )
+    end)
+  end
+
+  def find_non_intersecting(rectangles) do
+    Enum.find(rectangles, fn {outer_id, outer_area} ->
+      Enum.all?(rectangles, fn {inner_id, inner_area} ->
+        outer_id == inner_id || MapSet.disjoint?(outer_area, inner_area)
+      end)
     end)
   end
 end
@@ -72,44 +56,42 @@ case System.argv() do
     defmodule Day3Test do
       use ExUnit.Case
 
+      import Day3
+
       test "Part 1" do
         rectangles =
-          Day3.parse_input("""
+          parse_input("""
           #1 @ 1,3: 4x4
           #2 @ 3,1: 4x4
           #3 @ 5,5: 2x2
           """)
 
-        assert rectangles |> Day3.find_intersecting_area() |> MapSet.size() == 4
+        assert rectangles |> find_intersecting_area() |> MapSet.size() == 4
       end
 
       test "Part 2" do
         rectangles =
-          Day3.parse_input("""
+          parse_input("""
           #1 @ 1,3: 4x4
           #2 @ 3,1: 4x4
           #3 @ 5,5: 2x2
           """)
 
-        intersecting_area = rectangles |> Day3.find_intersecting_area()
-
         assert rectangles
-               |> Day3.reject_intersecting(intersecting_area)
-               |> List.first()
-               |> Map.get(:id) == 3
+               |> find_non_intersecting()
+               |> elem(0) == 3
       end
     end
 
   [input_file] ->
     rectangles = input_file |> File.read!() |> Day3.parse_input()
 
-    intersecting_area = rectangles |> Day3.find_intersecting_area()
+    intersecting_area = rectangles |> Day3.find_intersecting_area() |> MapSet.size()
 
-    non_intersecting_rectangle_id =
-      rectangles |> Day3.reject_intersecting(intersecting_area) |> List.first() |> Map.get(:id)
+    non_intersecting_rectangle_id = rectangles |> Day3.find_non_intersecting() |> elem(0)
 
     IO.puts("""
-    Part1: #{MapSet.size(intersecting_area)}
+    Part1: #{intersecting_area}
     Part2: #{non_intersecting_rectangle_id}
     """)
 end
